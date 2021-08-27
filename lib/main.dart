2c6d37954a29ee636322f778ceecd61f2853c8e0
Hanna Sabu Mampallil <hanblessab@gmail.com>
2021-08-28 05:18:51 +0530

    home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:sharkhack/home.dart';
-import 'package:sharkhack/new_report.dart';
-
+//import 'package:sharkhack/new_report.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: NewReportPage(),
+    home: HomePage(),
   ));
 }
